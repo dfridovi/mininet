3d1ae02361599186506fa366bf7b18edd1acedae
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2016-12-14 11:31:32 -0800

    first commit

---
+++ data/compare_explorers.m
@@ -0,0 +1,42 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Author: David Fridovich-Keil
+% File: compare_explorers.m
+%
+% Usage: Set the file names for the RW and LP recorded data and run.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all; close all;
+
+rw_data = csvread('size10x10_srcs2_steps1_samples1k_rw.csv');
+lp_data = csvread('size10x10_srcs2_steps1_samples1k_lp.csv');
+socp_data = csvread('size10x10_srcs2_steps1_samples1k_socp.csv');
+
+num_iterations = size(lp_data, 2);
+
+socp_means = zeros(1, num_iterations); socp_stddevs = zeros(1, num_iterations);
+lp_means = zeros(1, num_iterations); lp_stddevs = zeros(1, num_iterations);
+rw_means = zeros(1, num_iterations); rw_stddevs = zeros(1, num_iterations);
+
+for jj = 1:num_iterations
+    socp_means(jj) = mean(socp_data(:, jj)); 
+    socp_stddevs(jj) = std(socp_data(:, jj));
+    lp_means(jj) = mean(lp_data(:, jj)); 
+    lp_stddevs(jj) = std(lp_data(:, jj));
+    rw_means(jj) = mean(rw_data(:, jj));
+    rw_stddevs(jj) = std(rw_data(:, jj));
+end
+
+figure;
+hold on;
+grid on;
+errorbar((0:(num_iterations-1)), socp_means, 1.0 * socp_stddevs, 'kX', 'MarkerSize', 10);
+errorbar((0:(num_iterations-1)) - 0.2, lp_means, 1.0 * lp_stddevs, 'r*', 'MarkerSize', 10);
+errorbar((0:(num_iterations-1)) + 0.2, rw_means, 1.0 * rw_stddevs, 'bo', 'MarkerSize', 10);
+xlim([-0.5, num_iterations-0.5]);
+set(gca, 'fontsize', 24);
+legend('SOCP explorer', 'LP explorer', 'RW explorer');
+xlabel('Time step');
+ylabel('Entropy (nats)');
+title(sprintf('Empirical Comparison of (Robust) Information Maximization \n and a Random Walk'));
---
+++ include/layer/layer.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2015, The Regents of the University of California (Regents).
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    1. Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *    3. Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS AS IS
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Please contact the author(s) of this library if you have any questions.
+ * Author: David Fridovich-Keil   ( dfk@eecs.berkeley.edu )
+ *         Sara Fridovich-Keil    ( saraf@princeton.edu )
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Defines the Layer base class.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef MININET_LAYER_LAYER_H
+#define MININET_LAYER_LAYER_H
+
+#include <util/types.h>
+
+namespace mininet {
+
+class Layer {
+public:
+  explicit Layer(size_t input_size, size_t output_size);
+  virtual ~Layer();
+
+  // Get input/output sizes and weights.
+  inline size_t InputSize() const;
+  inline size_t OutputSize() const;
+  inline const MatrixXd& ImmutableWeights() const;
+
+  // Activation and gradient. Implement these in derived classes.
+  virtual void Activation(const VectorXd& input, VectorXd& output) const = 0;
+  virtual void Gradient(const VectorXd& upstream, VectorXd* gradient) const = 0;
+
+private:
+  // Weights from input (with bias) to output.
+  MatrixXd weights;
+
+}; // class Layer
+
+} // namespace mininet
+
+#endif
