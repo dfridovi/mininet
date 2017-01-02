fc3cbf92c96bac24cfab4a6f42186dc28fc283b9
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2017-01-02 15:11:53 -0500

    Automated documentation build for changeset 3110ff6b5340ca74abe7444634ffcf2425b1c53c.

---
+++ documentation/html/search/all_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['outputlayer',['OutputLayer',['../classmininet_1_1_output_layer.html',1,'mininet']]]
+];
---
+++ documentation/publish_documentation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+
+# Quit on errors.
+set -o errexit -o nounset
+
+# Settings.
+DOCUMENTATION_PATH=documentation
+CHANGESET=$(git rev-parse --verify HEAD)
+
+# Make documentation.
+doxygen Doxyfile
+
+# Make sure branches are up to date.
+git remote set-branches --add origin gh-pages
+git fetch origin
+
+# Commit documentation in master.
+cd ..
+git add ${DOCUMENTATION_PATH}
+git commit -m "adding documentation"
+
+# Check out gh-pages branch and merge documentation from master commit.
+git checkout gh-pages
+rm -r ${DOCUMENTATION_PATH}
+git checkout master ${DOCUMENTATION_PATH}
+
+# Add the merged changes and push.
+git commit -a -m "Automated documentation build for changeset ${CHANGESET}."
+git push -u origin gh-pages
+
+# Checkout master again and blow away generated docs.
+git checkout master
+git reset HEAD~1
+rm -r ${DOCUMENTATION_PATH}/html/ ${DOCUMENTATION_PATH}/latex/
+
+echo "-- Successfully updated documentation!"
