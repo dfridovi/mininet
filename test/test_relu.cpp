1265b1af5a350e3e54c45c4f45d0b1c2cf229240
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2017-01-05 13:17:16 -0800

    writing relu test

---
+++ test/test_relu.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2017, David Fridovich-Keil.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    1. Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *    3. Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS AS IS
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Please contact the author(s) of this library if you have any questions.
+ * Authors: David Fridovich-Keil   ( dfk@eecs.berkeley.edu )
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Unit tests for the ReLU layer type. The main idea here is to test whether a
+// network composed of only ReLU layers can compute derivatives properly.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include <layer/relu.h>
+#include <layer/layer_params.h>
+#include <loss/loss_functor.h>
+#include <net/network.h>
+
+#include <gtest/gtest.h>
+#include <vector>
+#include <random>
+#include <iostream>
+#include <math.h>
+
+using namespace mininet;
+
+// Single layer test.
+TEST(ReLU, TestSingleLayer) {
+  const size_t kNumInputs1 = 10;
+  const size_t kNumInputs2 = 3;
+  const size_t kNumOutputs = 3;
+
+  // Create a network.
+  const std::vector<LayerParams> params;
+  params.push_back(LayerParams(RELU, kNumInputs1, kNumInputs2));
+  params.push_back(LayerParams(SOFTMAX, kNumInputs2, kNumOutputs));;
+
+  const LossFunctor::ConstPtr loss = CrossEntropy::Create();
+  Network net(params, loss);
+
+  // Generate a random input/output pair.
+  const VectorXd input
+}
